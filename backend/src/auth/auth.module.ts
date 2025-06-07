781d8636b9fa76967a84a59e3302ec0610eade8e
Jungu Lee <prnsslcn@gmail.com>
2025-06-07 14:42:25 +0900

    feat: AuthModule 구성 및 Google/JWT 전략 모듈 등록

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { GoogleStrategy } from './strategies/google.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UsersModule } from 'src/users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                secret: config.get('JWT_SECRET'),
+                signOptions: { expiresIn: '7d' },
+            }),
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [AuthService, GoogleStrategy, JwtStrategy],
+})
+export class AuthModule {}
